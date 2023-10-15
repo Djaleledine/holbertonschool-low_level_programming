f0ce47b919ec3367408f06322852e40d7f8995f2
Djaleledine <menadijallal@gmail.com>
2023-10-15 10:35:22 -0700

    puts_fixed

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-
+/**
+ * main - this is the main the function of this program
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
