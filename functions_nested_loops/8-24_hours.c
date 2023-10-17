378a81948b64988fcc44b757cb7c5a9f9b4abbeb
Djaleledine <menadijallal@gmail.com>
2023-10-17 05:43:52 -0700

    Jack_bauer

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of a day
+ *
+ *
+ * Return: void
+ */
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	a = 0;
+	b = 0;
+	c = 0;
+	d = 0;
+	while (a <= 2)
+	{
+		while (c <= 5)
+		{
+			if (d <= 9)
+			{
+				_putchar('0' + a);
+				_putchar('0' + b);
+				_putchar(':');
+				_putchar('0' + c);
+				_putchar('0' + d);
+				_putchar('\n');
+				d = d + 1;
+			}
+			else
+			{
+				d = 0
+				c = c + 1;
+			}
+		}
+		c = 0;
+		if (a < 2)
+		{
+			if (b <= 9)
+			{
+				b = b + 1;
+			}
+			else
+			{
+				b = 0;
+				a = a + 1;
+			}
+		}
+		else
+		{
+			if (b <= 3)
+			{
+				b++;
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+}
