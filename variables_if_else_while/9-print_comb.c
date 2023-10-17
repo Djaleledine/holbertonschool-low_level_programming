a7353752f4f4f5f59a6cdb65fa13d83e61304385
Djaleledine <menadijallal@gmail.com>
2023-10-17 01:03:53 -0700

    print_comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase followed by a new line
+ * using the putchar function.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+	{
+		putchar('0' + c);
+		if (c != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
