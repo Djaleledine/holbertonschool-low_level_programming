39e8adc2637c311d54080726acde0588b400dd25
Djaleledine <menadijallal@gmail.com>
2023-10-17 00:46:47 -0700

    print_Numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase followed by a new line
+ * using the putchar function.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c <= 9; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
