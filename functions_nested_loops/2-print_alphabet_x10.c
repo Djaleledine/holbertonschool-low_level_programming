ccc59cb7750a0fa5ccd7fbff5b657522cc1755da
Djaleledine <menadijallal@gmail.com>
2023-10-17 02:14:43 -0700

    alphabet10Times

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -12,11 +12,9 @@ void print_alphabet_x10(void)
 {
 	char c;
 	int i;
-	
+
 	for (i = 0; i < 10; i++)
 	{
-		for (c = 'a'; c <= 'z'; c++)
-			_putchar(c);
+		print_alphabet();
 	}
-	_putchar('\n');
 }
