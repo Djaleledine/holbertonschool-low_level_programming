c6bf1b3ec27a52a196742f6278af7f3314f7b88a
Djaleledine <menadijallal@gmail.com>
2023-10-18 02:51:54 -0700

    Add

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * print_int - return the sum of two numbers
+ * @n: first number
+ * @n1: second number
+ * Description: return the sum of
+ * two integers
+ *
+ *
+ * Return: the sum of n + n1
+ */
+
 void print_int(int num)
 {
+	if (num < 0)
+	{
+		_putchar('-');
+		num = num * -1;
+	}
+	if (num == 0)
+	{
+		_putchar('0');
+	}
+
 	if (num / 10)
-		print_int(num);
+		print_int(num / 10);
 	_putchar('0' + (num % 10));
 }
 /**
@@ -19,17 +40,8 @@ void print_int(int num)
 int add(int n, int n1)
 {
 	int sum;
+	
 	sum = n + n1;
-	if (sum < 0)
-	{
-		_putchar('-');
-		sum = sum * -1;
-	}
-	if (sum == 0)
-	{
-		_putchar('0');
-		return(sum);
-	}
 	print_int(sum);
 	return (sum);
 }
