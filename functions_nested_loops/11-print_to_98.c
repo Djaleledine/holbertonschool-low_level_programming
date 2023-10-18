6bce19773fc7a5963a6af7a65a0b7b1f19c4520b
Djaleledine <menadijallal@gmail.com>
2023-10-18 03:16:46 -0700

    till_98_fixed

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,24 +10,27 @@
  */
 void print_to_98(int n)
 {
-	printf("%d", n);
 	if (n > 98)
 	{
+		printf("%d", n);
 		while (n > 98)
 		{
 			n = n - 1;
 			printf(", %d", n);
 		}
+		_putchar('\n')
 	} else if (n < 98)
 	{
+		printf("%d", n)
 		while (n < 98)
 		{
 			n = n + 1;
 			printf(", %d", n);
 		}
+		_putchar('\n')
 	} else
 	{
-		printf("98"); 
+		printf("98\n"); 
 	}
 	
 }
