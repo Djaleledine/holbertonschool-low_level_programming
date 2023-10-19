a0d87a17d63eb5f35d7ea92ac888e39bd3b89f76
Djaleledine <menadijallal@gmail.com>
2023-10-19 05:39:18 -0700

    num

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - description
+ * Description
+ *
+ * Return: print numbers
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar('0' + n);
+	}
+	_putchar('\n')
+}
